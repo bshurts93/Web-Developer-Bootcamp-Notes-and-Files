f2058c9c7f807de8bf8f99389e36cb66483a45ce
Ben <bshurts93@gmail.com>
2018-06-26 15:30:07 -0400

    Add files via upload
    I had a go at adding additional and advanced DOM manipulation to the stripe div. I wanted to change the selected class to display the picked color and toggle back to the regular selected class upon a restart, but am having trouble with a few bugs. Will hopefully find a solution soon.

--- The Web Developer Bootcamp/Section 13 - DOM Manipulation/Color Game/color-game.js
+++ The Web Developer Bootcamp/Section 13 - DOM Manipulation/Color Game/color-game.js
@@ -1,9 +1,72 @@
+var numSquares = 6;
 var colors = generateRandomColors(6);
 var squares = document.querySelectorAll(".square");
 var pickedColor = pickColor();
-var colorDisplay = document.getElementById("colorDisplay");
+var colorDisplay = document.getElementById("color-display");
 var messageDisplay = document.querySelector("#message");
 var h1 = document.querySelector("h1");
+var resetButton = document.querySelector("#reset");
+var easyButton = document.querySelector("#easy-btn");
+var hardButton = document.querySelector("#hard-btn");
+
+var selectedBtn = document.querySelector(".selected");
+
+
+easyButton.addEventListener("click", function(){
+    easyButton.classList.add("selected");
+    hardButton.classList.remove("selected");
+    numSquares = 3
+    colors = generateRandomColors(numSquares);
+    pickedColor = pickColor();
+    colorDisplay.textContent = pickedColor;
+    h1.style.backgroundColor = "steelblue";
+    for (var i = 0; i < squares.length; i++) {
+        if (colors[i]){
+            squares[i].style.backgroundColor = colors[i];
+        } else {
+            squares[i].style.display = "none";
+        }
+    }
+    // remove hardBtn background style
+    hardButton.style = "none";
+});
+
+hardButton.addEventListener("click", function(){
+    easyButton.classList.remove("selected");
+    hardButton.classList.add("selected");
+    numSquares = 6
+    colors = generateRandomColors(numSquares);
+    pickedColor = pickColor();
+    colorDisplay.textContent = pickedColor;
+    h1.style.backgroundColor = "steelblue";
+    for (var i = 0; i < squares.length; i++) {
+        squares[i].style.backgroundColor = colors[i];
+        squares[i].style.display = "block";
+    }
+    // remove easyBtn background style
+    easyButton.style = "none";
+});
+
+resetButton.addEventListener("click", function() {
+    // generate all new colors
+    colors = generateRandomColors(numSquares);
+    // pick a new random color from array
+    pickedColor = pickColor();
+    // change color display to match picked color
+    colorDisplay.textContent = pickedColor;
+    // change colors of all squares
+    for (var i = 0; i < squares.length; i++) {
+        squares[i].style.backgroundColor = colors[i];
+    }
+    h1.style.backgroundColor = "steelblue";
+    this.textContent = "New Colors"
+    messageDisplay.textContent = "";
+    
+    easyButton.style = "none";
+    hardButton.style = "none";
+    selectedBtn.style.backgroundColor = "steelblue";
+
+});
 
 
 colorDisplay.textContent = pickedColor;
@@ -21,11 +84,21 @@ for (var i = 0; i < squares.length; i++) {
         // compare color to clicked color
         if (clickedColor === pickedColor) {
             messageDisplay.textContent = "Correct!";
+            messageDisplay.style.fontFamily = "Avenir";
             changeColors(clickedColor);
             h1.style.backgroundColor = clickedColor;
+            resetButton.textContent = "Play Again?";
+            
+            // change seleceted to picked color
+            if (numSquares === 6) {
+                hardButton.style.backgroundColor = pickedColor;
+            } else {
+                easyButton.style.backgroundColor = pickedColor;
+            }
         } else {
             this.style.backgroundColor = "#232323"
             messageDisplay.textContent = "Try again!";
+            messageDisplay.style.fontFamily = "Avenir";
         }
     }); 
 }
